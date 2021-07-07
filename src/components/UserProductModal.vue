12832459ff69f55a72ae1a44ee827ceacdbed7c0
KimberlyIn <KimberlyIn@users.noreply.github.com>
2021-07-07 12:24:05 +0800

    210707

---
+++ src/components/UserProductModal.vue
@@ -0,0 +1,112 @@
+<template>
+
+	<!-- 查 tabindex="1" -->
+	<!-- 查 role="dialog" -->
+	<!-- 查 aria-labelledby="exampleModalLabel" -->
+	<!-- 查 aria-hidden="true" -->
+
+	<div
+	class="modal fade"
+	id="productModal"
+	tabindex="-1"
+	role="dialog"
+	aria-labelledby="exampleModalLabel"
+	aria-hidden="true"
+	ref="modal"
+	>
+		<div class="modal-dialog modal-xl" role="document">
+			<div class="modal-content border-0">
+				<div class="modal-header bg-dark text-white">
+					<h5 class="modal-title" id="exampleModalLabel">
+						<span>{{ product.title }}</span>
+					</h5>
+					<button
+						type="button"
+						class="btn-close"
+						data-bs-dismiss="modal"
+						aria-label="Close"
+					></button>
+				</div>
+				<div class="madal-body">
+					<div class="row">
+						<div class="col-sm-6">
+							<!-- 背景圖片 -->
+							<img class="img-fluid" :src="product.imagesUrl" alt="" />
+						</div>
+						<div class="col-sm-6">
+							<span class="badge bg-primary rounded-pill">
+								{{ product.category }}
+							</span>
+							<p>商品描述：{{ product.description }}</p>
+							<p>商品內容：{{ product.content }}</p>
+							<div class="h5" v-if="!product.price">
+								{{ product.origin_price }} 元
+							</div>
+							<div class="h6" v-if="product.price">
+								原價 {{ product.origin_price }} 元
+							</div>
+							<div class="h5" v-if="product.price">
+								現在只要 {{ product.price }} 元
+							</div>
+							<div>
+								<div class="input-group">
+									<input type="number"
+									class="form-control"
+									v-model.number="qty"
+									min="1" />
+									<button
+										type="button"
+										class="btn btn-primary"
+										@click="$emit('add-to-cart', product.id, qty)">
+										加入購物車
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import Modal from 'bootstrap/js/dist/modal';
+
+export default {
+	// 資料
+	props: {
+		product: {
+			// 不太清楚是放哪裡的資料
+			type: Object,
+			default() {
+				return {};
+			},
+		},
+	},
+	data() {
+		return {
+			status: {},
+			modal: '',
+			qty: 1,
+		};
+	},
+	// 執行方法
+	mounted() {
+		// 對應到 bootstrap 的 modal
+		this.modal = new Modal(this.$refs.modal,{
+			keybodal: false,
+			backdrop:'static'
+		});
+	},
+	// 把結果執行在畫面上
+	methods:{
+		openModal() {
+			this.modal.show();
+		},
+		hideModal() {
+			this.modal.hide();
+		},
+	},
+}
+</script>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,5 +1,166 @@
 <template>
-    <div>
-        <h1>歡迎來到 Jemma 的產品頁面</h1>
-    </div>
+	<div>
+		<h1>Jemma 的產品頁面</h1>
+		<Loading :active="isLoading"></Loading>
+		<!-- align-middle 查 -->
+		<table class="table align-middle">
+			<thead>
+				<tr>
+					<th>圖片</th>
+					<th>商品名稱</th>
+					<th>價格</th>
+					<th></th>
+				</tr>
+			</thead>
+			<tbody>
+				<!-- products 多筆渲染 -->
+				<tr v-for="item in products" :key="item.id">
+					<td style="width: 200px">
+						<div style="width:100px; background-size: cover; background-position: center"
+						:style="{ backgroundImage:`url(${item.imageUrl})` }">
+						<!-- 加入背景圖片，前面是屬性名稱，後面是值 -->
+						</div>
+					</td>
+					<td>
+						{{ item.title }}
+					</td>
+					<td>
+						<!-- 如果金額不等於 item.price 就渲染 item.origin_price -->
+						<!-- 當有輸入售價時，就把原價畫上刪除線、並顯示售價。如果沒有售價的話，就直接顯示原價 -->
+						<div class="h5" v-if="!item.price">{{ item.origin_price }} 元</div>
+						<div class="h6" v-if="item.price">原價 {{ item.origin_price }} 元</div>
+						<div class="h5" v-if="item.price">現在只要 {{ item.price }} 元</div>
+					</td>
+					<td>
+						<div class="btn-group btn-group-sm">
+							<!-- @click="getProduct(item.id)" v-on 點擊觸發函式 -->
+							<!-- :disabled="loadingStatus.loadingItem === item.id" 
+							如果 loadingStatus.loadingItem 等於 item.id 就啟動 disabled -->
+							<button
+								type="button"
+								class="btn btn-outline-secondary"
+								@click="getProduct(item.id)"
+								:disabled="loadingStatus.loadingItem === item.id">
+								<!-- v-if="lodingStatus.lodingItem === item.id" 
+								v-if 為條件判斷 這裡為 disabled 的判斷式 -->
+								<i class="fas fa-spinner fa-pulse"
+									v-if="lodingStatus.lodingItem === item.id"
+								></i>
+									查看更多
+							</button>
+							<button
+								type="button"
+								class="btn btn-outline-danger"
+								@click="addToCart(item.id)"
+								:disabled="loadingStatus.loadingItem === item.id">
+								<i class="fas fa-spinner fa-pulse"
+									v-if="loadingStatus.loadingItem === item.id"
+								></i>
+								加到購物車
+							</button>
+						</div>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+		<UserProductModal
+		ref="userProductModal"
+		:product="product"
+		@add-to-cart="addToCart">
+		</UserProductModal>
+	</div>
 </template>
+
+<script>
+// @ = src + 絕對路徑
+import UserProductModal from '@/components/UserProductModal.vue';
+
+export default {
+	name: 'Products',
+	data() {
+		return {
+			// 產品列表
+			products: [],
+			// disabled 判斷
+			loadingStatus: {
+				loadingItem: '',
+			},
+			// 問
+			isLoading: false,
+			product: {},
+		};
+	},
+	// 問
+	components: {
+		UserProductModal,
+	},
+	created() {
+		// 研究
+		this.getProducts();
+	},
+	methods: {
+		// 問
+		// 加入購物車
+		addToCart(id,qty = 1) {
+			// 問
+			this.isLoading = true;
+			// 問
+			const url = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/cart`;
+			// 阻止用戶一直去點擊 loadingStatus.loadingItem 如果等於 id 就會變成 disabled 的狀態
+			this.loadingStatus.loadingItem = id;
+			// 購物車結構
+			const cart = {
+				product_id: id,
+				qty,
+			};
+			// [參數]: { "data": { "product_id":"-L9tH8jxVb2Ka_DYPwng","qty":1 } }
+			this.$http.post(url, { data: cart})
+			.then((response) => {
+				// console.log(response);
+				if(response.data.success) {
+					alert(response.data.message);
+					// 資料取得完成以後 disabled 狀態才會消失
+					this.loadingStatus.loadingItem = '';
+					this.$refs.UserProductModal.hideMod();
+					this.isLoading = false;
+				}	else {
+					alert(response.data.message);
+				}
+			});
+		},
+		// 問 取得所有產品細節？
+		getProucts() {
+			this.isLoading = true;
+			const url = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/products`
+			// 問 $http 是什麼 這裡得 this 會直接指向 axios ?
+			this.$http.get(url)
+			.then((response) => {
+				if(response.data.success) {
+					// 回傳 response.data 裡面的 products
+					this.products = response.data.products;
+					this.isLoading = false;
+				} else {
+					alert(response.data.message);
+				}
+			});
+		},
+		// 取得單一商品細節
+		getProuct(id) {
+			this.isLoading = true;
+			const url = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/product/${id}`;
+			this.loadingStatus.loadingItem = id;
+			this.$http.get(url)
+			.then((response) => {
+				if(response.data.success) {
+					this.loadingStatus.loadingItem = '';
+					this.product = response.data.product;
+					this.isLoading = false;
+					this.$refs.UserProductModal.openModal();
+				} else {
+					alert(response.data.message);
+				}
+			});
+		},
+	}
+}
+</script>
